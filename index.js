fd7c3a75a9584bb13fd7730b363de16b0d67eb01
Paaaaaaul <paul.meyer@live.de>
2017-10-14 20:46:08 -0500

    refactored api with alexa-sdk

--- index.js
+++ index.js
@@ -1,148 +1,49 @@
-//var Firebase = require("firebase");
-//import Firebase from './firebase'
-var Firebase = require("firebase")
+'use strict';
+var Alexa = require('alexa-sdk');
+var firebase = require('firebase');
+
+var APP_ID ='amzn1.ask.skill.36e138ee-5e0d-46a7-a9cd-9104e28d892d';
+var SKILL_NAME = 'diactate';
+var PROJECT_TITLE = "You can create a new project now,please create a name";
+var ERROR_REPROMPT = 'Sorry I can not understand what you are saying';
+
 var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-      apiKey: 'AIzaSyDeOoXMfCIt2-WXJ6mLp3TcCQxcGK-lp38',
-      authDomain: 'dictate-244d5.firebaseapp.com',
-      databaseURL: 'https://dictate-244d5.firebaseio.com',
-      storageBucket: 'dictate-244d5'
-    }
-var FIRE =  Firebase.initializeApp(config);
-function buildSpeechletResponse(output,repromptText,shouldEndSession){
-    return {
-        outputSpeech: {
-            type: 'PlainText',
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: 'PlainText',
-                text: repromptText,
-            },
-        },
-        shouldEndSession,
-    } 
-}
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: '1.0',
-        sessionAttributes,
-        response: speechletResponse,
-    };
-}
-function getWelcome(callback) {
-    // If we wanted to initialize the session to have some attributes we could add those here.
-    const sessionAttributes = {};
-    const speechOutput = 'Welcome to The Dictator. ' +
-        'start a new project or open an existing project';
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
-    const repromptText = 'Please tell me what to do by saying, start new project or open project';
-    const shouldEndSession = false;
+    apiKey: 'AIzaSyDeOoXMfCIt2-WXJ6mLp3TcCQxcGK-lp38',
+    authDomain: 'dictate-244d5.firebaseapp.com',
+    databaseURL: 'https://dictate-244d5.firebaseio.com',
+    storageBucket: 'dictate-244d5'
+  }
+var FIRE =  firebase.initializeApp(config);
+var database = firebase.database();
 
-    callback(sessionAttributes,
-        buildSpeechletResponse(speechOutput, repromptText, shouldEndSession));
-}
-function getTitleObj(projectTitle){
-    return {
-        projectTitle,
-    }
-}
-
-function createNewProject(intent,session,callback){
-    let projectTitle = intent.slots.Title.value;
-    //log intent to blackbox
-    console.log(intent);
-
-    let sessionAttributes = {};
-    let speachOutput = '';
-    let repromptText = '';
-    const shouldEndSession = true;
-    
-    // log project title to blackbox
-    console.log("Project Title is ", projectTitle);
-    
-    if(projectTitle){
-        sessionAttributes = getTitleObj(projectTitle);
-        speechOutput = `The name of the project is ${projectTitle}`;
-    }
-    else
-    {
-        speechOutput = 'No valid title given'
-    }
-
-    callback(sessionAttributes, buildSpeechletResponse(speechOutput, repromptText, shouldEndSession));
-
-    
-    console.log('this shit loads here');
-    FIRE.database().ref('projects/1/project').set({
-        active: true,
-        title: projectTitle
-    });
-    callDB();
-}
-function callDB (){
-     FIRE.database().ref('projects').on( 'value', (snapshot) =>{
-        snapshot.forEach((child) => {
-            console.log(child.val().project);
-        })
-    });
-}
-
-function onIntent(intentRequest, session, callback){
-    console.log("IntentRequest: ", intentRequest);
-    
-    const intent = intentRequest.intent;
-    const intentName = intentRequest.intent.name;
-    
-    console.log("Intent Name is",intentName);
-    if(intentName === 'createProject')
-    {
-        createNewProject(intent,session,callback);
-    }
-    
-    
-}
-function onLaunch(launchRequest, session, callback)
-{
-    console.log('Alexa launch')
-    getWelcome(callback);
-}
-
-
-exports.handler = (event, context, callback) => {
-    // TODO implement
-     // then click "add firebase to your web app"
-     /*var config = { 
-        apiKey: 'AIzaSyDeOoXMfCIt2-WXJ6mLp3TcCQxcGK-lp38',
-        authDomain: 'dictate-244d5.firebaseapp.com',
-        databaseURL: 'https://dictate-244d5.firebaseio.com',
-        storageBucket: 'dictate-244d5'
-      }
-    Firebase.initializeApp(config);
-    const db = Firebase.database();*/
-    try{
-        if(event.session.new){
-            console.log("New Session started");
-        }
-        if(event.request.type ==='LaunchRequest')
-        {
-            // function calling welcome
-            onLaunch(event.request, event.session, (sessionAttributes, speechletResponse) => {
-                callback(null, buildResponse(sessionAttributes,speechletResponse));
-            })
-        } else if (event.request.type === 'IntentRequest') {
-            onIntent(event.request, event.session,(sessionAttributes, speechletResponse) => {
-                    callback(null, buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === 'SesionEndedRequest') {
-            onSessionEnded(event.request,event.session)
-            callback();
-        }
-        
-    } catch(err)
-    {
-        callback(err);
-    }
-    
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
+var handlers ={
+      'LaunchRequest': function () {
+        var greeting = 'Welcome to dictator, create a new project';
+        this.emit(':ask', greeting, 'Say, create new project');
+      },
+    'GetNewProject': function(){
+     
+        var projectTitle = this.event.request.intent.slots.Title.value;     
+
+        FIRE.database().ref('/projects/projects/').push().set({
+            project: {
+                active:false,
+                title: projectTitle
+            }
+          }, () => {
+            this.emit(':tellWithCard', 'Your new project title is '+ projectTitle, SKILL_NAME, 'what should we do next?')      
+        });
+
+      console.log('project title: ' + projectTitle);
+     // this.emit(':tell', 'Your new project title is '+ projectTitle, 'what should we do next?');
+      //contact firebase function;
+    }
+};
+    
+    
